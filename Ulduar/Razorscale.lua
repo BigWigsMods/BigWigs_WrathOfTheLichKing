a00c64e44ba159c9e2e3d88f95e88041f29c62ba
funkydude <funkydude@users.noreply.github.com>
2023-01-22 21:25:38 +0000

    Ulduar/Razorscale: Fix an error on Fuse Armor

--- Ulduar/Razorscale.lua
+++ Ulduar/Razorscale.lua
@@ -158,6 +158,6 @@ end
 function mod:FuseArmor(args)
 	if self:Me(args.destGUID) or (self:Tank() and self:Tank(args.destName)) then
 		local amount = args.amount or 1
-		self:StackMessageOld(args.spellId, args.destName, amount, "orange", args.amount > 1 and "info")
+		self:StackMessageOld(args.spellId, args.destName, amount, "orange", amount > 1 and "info")
 	end
 end
