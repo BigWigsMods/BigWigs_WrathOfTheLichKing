bcf1dcca2c40260eb78bcb4317c7617fd2a7721a
funkydude <funkydude@users.noreply.github.com>
2023-01-29 17:31:25 +0000

    Ulduar/Hodir: Fix Hard Mode timer on classic

--- Ulduar/Hodir.lua
+++ Ulduar/Hodir.lua
@@ -54,7 +54,7 @@ end
 function mod:OnEngage()
 	lastCold = 0
 	self:Bar(61968, 35) -- Flash Freeze
-	self:Bar("hardmode", 150, CL.hard, 27578) -- ability_warrior_battleshout / Battle Shout / icon 132333
+	self:Bar("hardmode", self:Classic() and 121 or 150, CL.hard, 27578) -- ability_warrior_battleshout / Battle Shout / icon 132333
 	self:Berserk(480)
 end
 
