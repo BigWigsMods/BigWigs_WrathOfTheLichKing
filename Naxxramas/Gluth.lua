647e57a3b1b1fec45ca4c9f8a4a33bec592728ad
funkydude <funkydude@users.noreply.github.com>
2023-06-02 01:54:11 +0100

    Naxxramas/Gluth: Update use of :Error API

--- Naxxramas/Gluth.lua
+++ Naxxramas/Gluth.lua
@@ -71,7 +71,7 @@ function mod:MortalWoundApplied(args)
 		self:PlaySound(54378, "warning")
 	end
 	if args.spellId == 25646 then
-		BigWigs:Error("Mortal Wound found ID 25646 tell the BigWigs Devs! ".. self:Difficulty() .." ".. (self:Classic() and "classic" or "retail"))
+		self:Error("Mortal Wound found ID 25646 tell the devs! ".. self:Difficulty() .." ".. (self:Classic() and "classic" or "retail"))
 	end
 end
 
