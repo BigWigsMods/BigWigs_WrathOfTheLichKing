90d865e4a96dbf3bacfc3b553418e6c5e2b7ca01
funkydude <funkydude@users.noreply.github.com>
2024-01-15 15:47:36 +0000

    RubySanctum/Trash: Import trash module

--- RubySanctum/Locales/esES.lua
+++ RubySanctum/Locales/esES.lua
@@ -3,3 +3,12 @@ if not L then return end
 if L then
 	L.twilight_cutter_emote_trigger = "esferas" -- ¡Las esferas que orbitan emiten energía oscura!
 end
+
+L = BigWigs:NewBossLocale("The Ruby Sanctum Trash", "esES")
+if L then
+	L.baltharus = "Baltharus el Batallante" -- NPC 39751
+	L.saviana = "Saviana Furia Ardiente" -- NPC 39747
+	L.zarithrian = "General Zarithrian" -- NPC 39746
+
+	L.adds_yell_trigger = "¡Reducidlos a cenizas, esbirros!"
+end
--- RubySanctum/Locales/frFR.lua
+++ RubySanctum/Locales/frFR.lua
@@ -3,3 +3,12 @@ if not L then return end
 if L then
 	L.twilight_cutter_emote_trigger = "sphères" -- Les sphères volantes rayonnent d’énergie noire !
 end
+
+L = BigWigs:NewBossLocale("The Ruby Sanctum Trash", "frFR")
+if L then
+	L.baltharus = "Baltharus l'Enfant de la guerre" -- NPC 39751
+	L.saviana = "Saviana Ragefeu" -- NPC 39747
+	L.zarithrian = "Général Zarithrian" -- NPC 39746
+
+	L.adds_yell_trigger = "Serviteurs, réduisez-les en cendres !"
+end
